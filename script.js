8296267ead297c5b3d795de452ab9f9be412e097
CAPO2004 <capoca2004@gmail.com>
2025-05-12 01:55:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,69 +43,67 @@ const verses = {
     ]
 };
 
-// تعريف الروابط الصوتية بناءً على السورة والآية
+// تعريف مسارات الملفات الصوتية المحلية لكل آية
 const audioUrls = {
     "ياسر الدوسري": {
-        "آل عمران: 159": "https://www.mp3quran.net/eng/play/3/yasser_al_dosari/003159.mp3",
-        "النجم: 43": "https://www.mp3quran.net/eng/play/53/yasser_al_dosari/053043.mp3",
-        "الأعراف: 156": "https://www.mp3quran.net/eng/play/7/yasser_al_dosari/007156.mp3",
-        "يونس: 58": "https://www.mp3quran.net/eng/play/10/yasser_al_dosari/010058.mp3",
-        "الحجر: 87": "https://www.mp3quran.net/eng/play/15/yasser_al_dosari/015087.mp3",
-        "الرعد: 28": "https://www.mp3quran.net/eng/play/13/yasser_al_dosari/013028.mp3",
-        "الشرح: 6": "https://www.mp3quran.net/eng/play/94/yasser_al_dosari/094006.mp3",
-        "الطلاق: 2": "https://www.mp3quran.net/eng/play/65/yasser_al_dosari/065002.mp3",
-        "الشعراء: 217": "https://www.mp3quran.net/eng/play/26/yasser_al_dosari/026217.mp3",
-        "التوبة: 51": "https://www.mp3quran.net/eng/play/9/yasser_al_dosari/009051.mp3",
-        "الحديد: 4": "https://www.mp3quran.net/eng/play/57/yasser_al_dosari/057004.mp3",
-        "البقرة: 153": "https://www.mp3quran.net/eng/play/2/yasser_al_dosari/002153.mp3",
-        "التوبة: 40": "https://www.mp3quran.net/eng/play/9/yasser_al_dosari/009040.mp3",
-        "الطور: 48": "https://www.mp3quran.net/eng/play/52/yasser_al_dosari/052048.mp3",
-        "الزمر: 36": "https://www.mp3quran.net/eng/play/39/yasser_al_dosari/039036.mp3",
-        "إبراهيم: 7": "https://www.mp3quran.net/eng/play/14/yasser_al_dosari/014007.mp3",
-        "البقرة: 172": "https://www.mp3quran.net/eng/play/2/yasser_al_dosari/002172.mp3",
-        "المائدة: 7": "https://www.mp3quran.net/eng/play/5/yasser_al_dosari/005007.mp3",
-        "البقرة: 152": "https://www.mp3quran.net/eng/play/2/yasser_al_dosari/002152.mp3",
-        "آل عمران: 145": "https://www.mp3quran.net/eng/play/3/yasser_al_dosari/003145.mp3",
-        "آل عمران: 139": "https://www.mp3quran.net/eng/play/3/yasser_al_dosari/003139.mp3",
-        "يوسف: 87": "https://www.mp3quran.net/eng/play/12/yasser_al_dosari/012087.mp3",
-        "الأعراف: 156": "https://www.mp3quran.net/eng/play/7/yasser_al_dosari/007156.mp3",
-        "البقرة: 155": "https://www.mp3quran.net/eng/play/2/yasser_al_dosari/002155.mp3",
-        "آل عمران: 134": "https://www.mp3quran.net/eng/play/3/yasser_al_dosari/003134.mp3",
-        "الشورى: 37": "https://www.mp3quran.net/eng/play/42/yasser_al_dosari/042037.mp3",
-        "الأعراف: 199": "https://www.mp3quran.net/eng/play/7/yasser_al_dosari/007199.mp3",
-        "فصلت: 34": "https://www.mp3quran.net/eng/play/41/yasser_al_dosari/041034.mp3",
-        "الشورى: 40": "https://www.mp3quran.net/eng/play/42/yasser_al_dosari/042040.mp3"
+        "آل عمران: 159": "./audio/al_imran_159.mp3",
+        "النجم: 43": "./audio/an_najm_43.mp3",
+        "الأعراف: 156": "./audio/al_araf_156.mp3",
+        "يونس: 58": "./audio/yunus_58.mp3",
+        "الحجر: 87": "./audio/al_hijr_87.mp3",
+        "الرعد: 28": "./audio/ar_rad_28.mp3",
+        "الشرح: 6": "./audio/ash_sharh_6.mp3",
+        "الطلاق: 2": "./audio/at_talaq_2.mp3",
+        "الشعراء: 217": "./audio/ash_shuara_217.mp3",
+        "التوبة: 51": "./audio/at_tawbah_51.mp3",
+        "الحديد: 4": "./audio/al_hadid_4.mp3",
+        "البقرة: 153": "./audio/al_baqarah_153.mp3",
+        "التوبة: 40": "./audio/at_tawbah_40.mp3",
+        "الطور: 48": "./audio/at_tur_48.mp3",
+        "الزمر: 36": "./audio/az_zumar_36.mp3",
+        "إبراهيم: 7": "./audio/ibrahim_7.mp3",
+        "البقرة: 172": "./audio/al_baqarah_172.mp3",
+        "المائدة: 7": "./audio/al_maidah_7.mp3",
+        "البقرة: 152": "./audio/al_baqarah_152.mp3",
+        "آل عمران: 145": "./audio/al_imran_145.mp3",
+        "آل عمران: 139": "./audio/al_imran_139.mp3",
+        "يوسف: 87": "./audio/yusuf_87.mp3",
+        "البقرة: 155": "./audio/al_baqarah_155.mp3",
+        "آل عمران: 134": "./audio/al_imran_134.mp3",
+        "الشورى: 37": "./audio/ash_shura_37.mp3",
+        "الأعراف: 199": "./audio/al_araf_199.mp3",
+        "فصلت: 34": "./audio/fussilat_34.mp3",
+        "الشورى: 40": "./audio/ash_shura_40.mp3"
     },
     "ناصر القطامي": {
-        "آل عمران: 159": "https://www.mp3quran.net/eng/play/3/nasser_al_qatami/003159.mp3",
-        "النجم: 43": "https://www.mp3quran.net/eng/play/53/nasser_al_qatami/053043.mp3",
-        "الأعراف: 156": "https://www.mp3quran.net/eng/play/7/nasser_al_qatami/007156.mp3",
-        "يونس: 58": "https://www.mp3quran.net/eng/play/10/nasser_al_qatami/010058.mp3",
-        "الحجر: 87": "https://www.mp3quran.net/eng/play/15/nasser_al_qatami/015087.mp3",
-        "الرعد: 28": "https://www.mp3quran.net/eng/play/13/nasser_al_qatami/013028.mp3",
-        "الشرح: 6": "https://www.mp3quran.net/eng/play/94/nasser_al_qatami/094006.mp3",
-        "الطلاق: 2": "https://www.mp3quran.net/eng/play/65/nasser_al_qatami/065002.mp3",
-        "الشعراء: 217": "https://www.mp3quran.net/eng/play/26/nasser_al_qatami/026217.mp3",
-        "التوبة: 51": "https://www.mp3quran.net/eng/play/9/nasser_al_qatami/009051.mp3",
-        "الحديد: 4": "https://www.mp3quran.net/eng/play/57/nasser_al_qatami/057004.mp3",
-        "البقرة: 153": "https://www.mp3quran.net/eng/play/2/nasser_al_qatami/002153.mp3",
-        "التوبة: 40": "https://www.mp3quran.net/eng/play/9/nasser_al_qatami/009040.mp3",
-        "الطور: 48": "https://www.mp3quran.net/eng/play/52/nasser_al_qatami/052048.mp3",
-        "الزمر: 36": "https://www.mp3quran.net/eng/play/39/nasser_al_qatami/039036.mp3",
-        "إبراهيم: 7": "https://www.mp3quran.net/eng/play/14/nasser_al_qatami/014007.mp3",
-        "البقرة: 172": "https://www.mp3quran.net/eng/play/2/nasser_al_qatami/002172.mp3",
-        "المائدة: 7": "https://www.mp3quran.net/eng/play/5/nasser_al_qatami/005007.mp3",
-        "البقرة: 152": "https://www.mp3quran.net/eng/play/2/nasser_al_qatami/002152.mp3",
-        "آل عمران: 145": "https://www.mp3quran.net/eng/play/3/nasser_al_qatami/003145.mp3",
-        "آل عمران: 139": "https://www.mp3quran.net/eng/play/3/nasser_al_qatami/003139.mp3",
-        "يوسف: 87": "https://www.mp3quran.net/eng/play/12/nasser_al_qatami/012087.mp3",
-        "الأعراف: 156": "https://www.mp3quran.net/eng/play/7/nasser_al_qatami/007156.mp3",
-        "البقرة: 155": "https://www.mp3quran.net/eng/play/2/nasser_al_qatami/002155.mp3",
-        "آل عمران: 134": "https://www.mp3quran.net/eng/play/3/nasser_al_qatami/003134.mp3",
-        "الشورى: 37": "https://www.mp3quran.net/eng/play/42/nasser_al_qatami/042037.mp3",
-        "الأعراف: 199": "https://www.mp3quran.net/eng/play/7/nasser_al_qatami/007199.mp3",
-        "فصلت: 34": "https://www.mp3quran.net/eng/play/41/nasser_al_qatami/041034.mp3",
-        "الشورى: 40": "https://www.mp3quran.net/eng/play/42/nasser_al_qatami/042040.mp3"
+        "آل عمران: 159": "./audio/al_imran_159_qatami.mp3",
+        "النجم: 43": "./audio/an_najm_43_qatami.mp3",
+        "الأعراف: 156": "./audio/al_araf_156_qatami.mp3",
+        "يونس: 58": "./audio/yunus_58_qatami.mp3",
+        "الحجر: 87": "./audio/al_hijr_87_qatami.mp3",
+        "الرعد: 28": "./audio/ar_rad_28_qatami.mp3",
+        "الشرح: 6": "./audio/ash_sharh_6_qatami.mp3",
+        "الطلاق: 2": "./audio/at_talaq_2_qatami.mp3",
+        "الشعراء: 217": "./audio/ash_shuara_217_qatami.mp3",
+        "التوبة: 51": "./audio/at_tawbah_51_qatami.mp3",
+        "الحديد: 4": "./audio/al_hadid_4_qatami.mp3",
+        "البقرة: 153": "./audio/al_baqarah_153_qatami.mp3",
+        "التوبة: 40": "./audio/at_tawbah_40_qatami.mp3",
+        "الطور: 48": "./audio/at_tur_48_qatami.mp3",
+        "الزمر: 36": "./audio/az_zumar_36_qatami.mp3",
+        "إبراهيم: 7": "./audio/ibrahim_7_qatami.mp3",
+        "البقرة: 172": "./audio/al_baqarah_172_qatami.mp3",
+        "المائدة: 7": "./audio/al_maidah_7_qatami.mp3",
+        "البقرة: 152": "./audio/al_baqarah_152_qatami.mp3",
+        "آل عمران: 145": "./audio/al_imran_145_qatami.mp3",
+        "آل عمران: 139": "./audio/al_imran_139_qatami.mp3",
+        "يوسف: 87": "./audio/yusuf_87_qatami.mp3",
+        "البقرة: 155": "./audio/al_baqarah_155_qatami.mp3",
+        "آل عمران: 134": "./audio/al_imran_134_qatami.mp3",
+        "الشورى: 37": "./audio/ash_shura_37_qatami.mp3",
+        "الأعراف: 199": "./audio/al_araf_199_qatami.mp3",
+        "فصلت: 34": "./audio/fussilat_34_qatami.mp3",
+        "الشورى: 40": "./audio/ash_shura_40_qatami.mp3"
     }
 };
 
@@ -138,9 +136,9 @@ playAudioBtn.addEventListener('click', () => {
     
     if (audioUrl) {
         audio.src = audioUrl;
-        audio.play().catch(error => alert('خطأ في تشغيل الصوت، تأكد من الرابط أو الاتصال بالإنترنت!'));
+        audio.play().catch(error => alert('خطأ في تشغيل الصوت، تأكد من وجود الملف في مجلد audio!'));
     } else {
-        alert('لا يوجد رابط صوتي متاح لهذه الآية، يرجى إضافة رابط صالح!');
+        alert('لا يوجد ملف صوتي متاح لهذه الآية، يرجى إضافة الملف في مجلد audio!');
     }
 });
 
